ba30e05ac1bc96044a0d7bb83960c2a6bfcdd28e
Patrick Knöbel <pknoe3lh@gmail.com>
2016-10-14 19:52:38 +0200

    update Termistor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -144,7 +144,7 @@
 // 52 is 200k thermistor - ATC Semitec 204GT-2 (1k pullup)
 // 55 is 100k thermistor - ATC Semitec 104GT-2 (Used in ParCan & J-Head) (1k pullup)
 
-#define TEMP_SENSOR_0 99
+#define TEMP_SENSOR_0 98
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_BED 99
